1ca776e9556ac337e4149e2f9dcdefc74336dd52
tushar vashishtha <vashishthatushar004@gmail.com>
2026-01-06 14:49:22 +0530

    terrene

---
+++ src/app/layout.js
@@ -0,0 +1,20 @@
+import "./globals.css";
+import ClientLayout from "@/client-layout";
+import TopBar from "@/components/TopBar/TopBar";
+
+export const metadata = {
+  title: "Terrene ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <ClientLayout>
+          <TopBar />
+          {children}
+        </ClientLayout>
+      </body>
+    </html>
+  );
+}
